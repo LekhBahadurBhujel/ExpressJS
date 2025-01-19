9662e1eace0626e9b6fd934ac2a98a7b5be3564d
Lekh Bhujel <lekhbdrbhujel@gmail.com>
2025-01-19 15:17:45 +0545

    new

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const app = express();
 app.get("/users", function(req, res) {
         return res.json(users)
     })
-app.get("/user", function(req, res) {
-    const html=`<ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-    </ul>`
-    res.send(html)
+// app.get("/user", function(req, res) {
+//     const html=`<ul>
+//     ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+//     </ul>`
+//     res.send(html)
+// })
+
+app.get("/users/:id",function(req,res){
+    const id=Number(req.params.id)
+    const user = users.find((user)=>user.id===id)
+    return res.json(user);
 })
 
 app.listen(3000);
